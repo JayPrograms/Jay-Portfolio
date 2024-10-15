af060c63f7705d39e88172fb32e78177f52968ef
Jay Patel <jp841331@gmail.com>
2024-10-14 20:38:13 -0400

    Fixed import bugs

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,3 @@
-import logo from "./images/logo.png";
 import bannerImg from "./images/Portrait.png";
 import projectOne from "./images/projects/GameWithMe.jpeg";
 import projectTwo from "./images/projects/SevaHub.png";
@@ -6,14 +5,10 @@ import projectThree from "./images/projects/MyCalCout.png";
 import projectFour from "./images/projects/GRCScanner.png";
 import courseOne from "./images/projects/MachineLearning.png";
 import courseTwo from "./images/projects/DeepLearning.png";
-import testimonialOne from "./images/testmonial/testimonialOne.png";
-import testimonialTwo from "./images/testmonial/testimonialTwo.png";
-import quote from "./images/testmonial/quote.png";
 import contactImg from "./images/contact/contactImg.png";
 import newBannerImg from "./images/Portrait.png";
 
 export {
-  logo,
   bannerImg,
   newBannerImg,
   projectOne,
@@ -22,8 +17,5 @@ export {
   projectFour,
   courseOne,
   courseTwo,
-  testimonialOne,
-  testimonialTwo,
-  quote,
   contactImg,
 };
