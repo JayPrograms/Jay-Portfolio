f37048f25a83c7b77c8766ce1ee2913bacda69bd
Jay Patel <jp841331@gmail.com>
2025-05-21 21:38:36 -0400

    Fixed formatting issues

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -14,7 +14,7 @@ const Education = () => {
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
           <h2 className="text-3xl md:text-4xl font-bold">Education</h2>
         </div>
-        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        <div className="mt-6 lgl:mt-14 w-full border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="BASc Computer Engineering (Honors)"
             subTitle="University of Waterloo (2023 - 2028)"
@@ -32,7 +32,7 @@ const Education = () => {
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
           <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
         </div>
-        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        <div className="mt-6 lgl:mt-14 w-full border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="AI Software Engineering Intern"
             subTitle="RamSoft - (May 2025 - Present)"
